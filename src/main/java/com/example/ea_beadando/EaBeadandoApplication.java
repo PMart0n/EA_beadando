b72e77de76f5639764e08348f7492dba4f6c39c0
PMart0n <pataki1996@gmail.com>
2025-11-15 09:20:25 +0100

    Initial commit for EA_beadando

---
+++ src/main/java/com/example/ea_beadando/EaBeadandoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ea_beadando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaBeadandoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EaBeadandoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ea_beadando/EaBeadandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ea_beadando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaBeadandoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
