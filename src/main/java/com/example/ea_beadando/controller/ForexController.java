e54e2972292f7f8ac636fe5f12f5174340179e44
pilisimartin <kfel04@hallgato.uni-neumann.hu>
2025-11-18 16:47:49 +0100

    implement Forex-account page with real Oanda API data

--- src/main/java/com/example/ea_beadando/controller/ForexController.java
+++ src/main/java/com/example/ea_beadando/controller/ForexController.java
@@ -1,6 +1,11 @@
 package com.example.ea_beadando.controller;
 
-import com.example.ea_beadando.model.forex.*;
+import com.example.ea_beadando.model.forex.MessageActPrice;
+import com.example.ea_beadando.model.forex.MessageClosePosition;
+import com.example.ea_beadando.model.forex.MessageHistPrice;
+import com.example.ea_beadando.model.forex.MessageOpenPosition;
+import com.example.ea_beadando.service.ForexService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +15,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class ForexController {
 
+    @Autowired
+    private ForexService forexService;
+
     // --- 1. Számlainformációk ---
     @GetMapping("/forex-account")
-    public String getAccountInfo(Model model) {
-        // TODO: API hívás
+    public String getAccountInfo(Model model) throws Exception {
+        model.addAttribute("account", forexService.getAccountSummary());
         model.addAttribute("contentFile", "forex/forex-account");
         return "layout";
     }
@@ -27,8 +35,9 @@ public class ForexController {
     }
 
     @PostMapping("/forex-aktar")
-    public String aktarResult(@ModelAttribute MessageActPrice messageActPrice, Model model) {
-        // TODO: API hívás
+    public String aktarResult(@ModelAttribute MessageActPrice messageActPrice, Model model) throws Exception {
+        model.addAttribute("price", forexService.getCurrentPrice(messageActPrice.getInstrument()));
+        model.addAttribute("instrument", messageActPrice.getInstrument());
         model.addAttribute("contentFile", "forex/forex-aktar-result");
         return "layout";
     }
@@ -42,8 +51,9 @@ public class ForexController {
     }
 
     @PostMapping("/forex-histar")
-    public String histarResult(@ModelAttribute MessageHistPrice messageHistPrice, Model model) {
-        // TODO: API hívás
+    public String histarResult(@ModelAttribute MessageHistPrice messageHistPrice, Model model) throws Exception {
+        model.addAttribute("candles",
+                forexService.getHistoricalPrices(messageHistPrice.getInstrument(), messageHistPrice.getGranularity()));
         model.addAttribute("contentFile", "forex/forex-histar-result");
         return "layout";
     }
@@ -57,16 +67,17 @@ public class ForexController {
     }
 
     @PostMapping("/forex-nyit")
-    public String nyitResult(@ModelAttribute MessageOpenPosition messageOpenPosition, Model model) {
-        // TODO:API hívás
+    public String nyitResult(@ModelAttribute MessageOpenPosition messageOpenPosition, Model model) throws Exception {
+        model.addAttribute("result",
+                forexService.openPosition(messageOpenPosition.getInstrument(), messageOpenPosition.getUnits()));
         model.addAttribute("contentFile", "forex/forex-nyit-result");
         return "layout";
     }
 
     // --- 5. Nyitott Pozíciók ---
     @GetMapping("/forex-poziciok")
-    public String getPoziciok(Model model) {
-        // TODO: API hívás
+    public String getPoziciok(Model model) throws Exception {
+        model.addAttribute("trades", forexService.listOpenTrades());
         model.addAttribute("contentFile", "forex/forex-poziciok");
         return "layout";
     }
@@ -80,8 +91,8 @@ public class ForexController {
     }
 
     @PostMapping("/forex-zar")
-    public String zarResult(@ModelAttribute MessageClosePosition messageClosePosition, Model model) {
-        // TODO: API hívás
+    public String zarResult(@ModelAttribute MessageClosePosition messageClosePosition, Model model) throws Exception {
+        model.addAttribute("result", forexService.closeTrade(messageClosePosition.getTradeId()));
         model.addAttribute("contentFile", "forex/forex-zar-result");
         return "layout";
     }
