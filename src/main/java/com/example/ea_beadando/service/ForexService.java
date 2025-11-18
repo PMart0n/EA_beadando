7950105d1d0aaf58fb5fcd35b5340771035c1dfa
pilisimartin <kfel04@hallgato.uni-neumann.hu>
2025-11-18 16:35:37 +0100

    add Oanda configuration and basic ForexService

---
+++ src/main/java/com/example/ea_beadando/service/ForexService.java
@@ -0,0 +1,51 @@
+package com.example.ea_beadando.service;
+
+import com.oanda.v20.Context;
+import com.oanda.v20.ContextBuilder;
+import com.oanda.v20.account.*;
+import com.oanda.v20.pricing.*;
+import com.oanda.v20.primitives.InstrumentName;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class ForexService {
+
+    private final Context ctx;
+    private final AccountID accountId;
+
+    public ForexService() {
+        String token = "//IDE KELL MAJD A TOKENEM";
+        String account = "//IDE KELL MAJD AZ ACCOUNT ID";
+
+        this.ctx = new ContextBuilder("https://api-fxpractice.oanda.com")
+                .setToken(token)
+                .setApplication("EA_beadando")
+                .build();
+
+        this.accountId = new AccountID(account);
+    }
+
+    // 1. Számlainformációk lekérése //
+    public AccountSummary getAccountSummary() throws Exception {
+        AccountContext accountContext = new AccountContext(ctx);
+        AccountSummaryResponse response = accountContext.summary(accountId);
+        return response.getAccount();
+    }
+
+    // 2. Aktuális ár lekérése //
+    public ClientPrice getCurrentPrice(String instrument) throws Exception {
+        PricingContext pricingContext = new PricingContext(ctx);
+
+        PricingGetResponse response = pricingContext.get(
+                accountId,
+                Collections.singleton(instrument)
+        );
+
+        if (!response.getPrices().isEmpty()) {
+            return response.getPrices().get(0);
+        }
+        return null;
+    }
+}
